0e6291f6bf796ef20bba5999e44c3ef3a0aa44cd
Tysen Imai-Toyama <42785023+TysenImaiToyama@users.noreply.github.com>
2019-04-26 23:54:30 -1000

    fix time to work with double digits

--- timer/views.py
+++ timer/views.py
@@ -8,20 +8,23 @@ import datetime
 @login_required(login_url="/accounts/login")
 def timer_view(request):
     user = request.user
-    ip = AddDevice.objects.get(owner=user).ip
+    ip = AddDevice.objects.filter(owner=user).first().ip
     bulb = Bulb(ip)
     startpause = False
     if request.GET.get('timerstart'):
         timeone = request.GET.get('timepicker-one')
         timetwo = request.GET.get('timepicker-two')
+        print(timetwo)
         def delZero(time):
             intver = int(time[0])
-            if intver < 10:
+            print(time[1])
+            if intver < 9 and time[1] is " ":
                 return "0" + time
             else:
                 return time
         timeone = delZero(timeone)
         timetwo = delZero(timetwo)
+        print(timetwo)
         while timetwo != datetime.datetime.now().strftime("%I : %M %p"):
             if timeone == datetime.datetime.now().strftime("%I : %M %p") and startpause is False:
                 bulb.turn_on()
