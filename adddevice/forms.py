de9c1f5a67930f6480a983dbb0743d66402d55d4
josefinafortin <josefina.l.fortin@gmail.com>
2019-04-29 18:36:26 -1000

    addition to forms.py

--- adddevice/forms.py
+++ adddevice/forms.py
@@ -2,6 +2,16 @@ from django import forms
 from . import models
 
 class AddNewDevice(forms.ModelForm):
+    ip = forms.CharField(
+            widget=forms.TextInput(
+                attrs={
+                    'placeholder': 'Enter Ip address'
+                }
+            )
+        )
+    
     class Meta:
         model = models.AddDevice
         fields = ['ip']
+        
+    
